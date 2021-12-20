e8ad7986601f010445b53234797cd0bb2b3ac7b7
Will Brennan <willbrennan314@gmail.com>
2021-12-20 15:41:06 -0500

    Completed assignment

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,9 @@
 class Character < ActiveRecord::Base
+    belongs_to :actors
+    belongs_to :shows
+
+    def say_that_thing_you_say
+        self.catchphrase
+    end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :networks
+
+
+    def actors_list 
+        self.actors.map do |actor|
+            actor.full_name
+        end
+    end
+
   
 end
